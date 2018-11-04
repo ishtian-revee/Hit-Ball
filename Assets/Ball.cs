b6505a99c2444f5061ddcb12d678a5877d424862
ishtian revee <ishtian11@gmail.com>
2018-11-04 17:22:52 +0600

    player movement and ball bouncing added

---
+++ Assets/Ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // configurable fields
+    public Vector2 startForce;
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb.AddForce(startForce, ForceMode2D.Impulse);
+	}
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+    // configerable fields
+    public float speed;
+    public Rigidbody2D rb;
+
+    // private fields
+    private float movement = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +15,13 @@ public class Player : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+	    movement = Input.GetAxis("Horizontal") * speed;
 	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + new Vector2(movement * Time.fixedDeltaTime, 0f));
+    }
 }
